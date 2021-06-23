234f6e52abf1a7604cfc910d12186f5efd63986b
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2021-06-23 13:37:47 -0500

    Initial commit

---
+++ src/Example.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export interface ExampleProps{
+    yourName: String
+}
+
+export function Example({yourName} : ExampleProps){
+    return <div>Hola mundo, {yourName}! </div>
+}
